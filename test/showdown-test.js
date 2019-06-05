3945d1f67f7e18f5455936c47a718321e9f8faf3
meisl <m.kling@sprax.org>
2019-06-05 14:48:19 +0200

    + tests for showdown makeMarkdown - need jsdom if running on Node

--- test/showdown-test.js
+++ test/showdown-test.js
@@ -4,6 +4,20 @@
 
 const expect = require('expect.js'),
 	showdown = require('showdown');
+	
+if (typeof module !== 'undefined') { // we're in Node
+	// see https://github.com/showdownjs/showdown/issues/691
+	let dom = new (require('jsdom')).JSDOM();
+	let ctor = showdown.Converter;
+	showdown.Converter = function (a, b, c, d) {
+		const instance = new ctor(a, b, c, d);
+		const _makeMarkdown = instance.makeMarkdown.bind(instance);
+		instance.makeMarkdown = function (html, doc) {
+			return _makeMarkdown(html, doc || dom.window.document);
+		};
+		return instance;
+	}
+}
 
 
 describe('showdown', function () {
@@ -14,8 +28,17 @@ describe('showdown', function () {
 		it('should be a function', function () {
 			expect(showdown.Converter).to.be.a(Function);
 		});
+		
 		const conv = new showdown.Converter({ noHeaderId: true }),
-			makeHtml = conv.makeHtml.bind(conv);
+			makeHtml = conv.makeHtml.bind(conv),
+			makeMarkdown = conv.makeMarkdown.bind(conv);
+			/*
+			function (html) {
+				const dom = new jsdom.JSDOM();
+
+				return conv.makeMarkdown(html, dom.window.document);
+			};
+			*/
 		describe('.makeHtml', function () {
 			it('should render md headings 1..6', function () {
 				expect(makeHtml('# Blah'		)).to.be('<h1>Blah</h1>');
@@ -25,6 +48,18 @@ describe('showdown', function () {
 				expect(makeHtml('##### Blah'	)).to.be('<h5>Blah</h5>');
 				expect(makeHtml('###### Blah'	)).to.be('<h6>Blah</h6>');
 				expect(makeHtml('####### Blah'	)).to.be('<h6># Blah</h6>');
+			});
+			
+		});
+		
+		describe('.makeMarkdown', function () {
+			it('should render html headings 1..6', function () {
+				expect(makeMarkdown('<h1>Blah</h1>'		)).to.be('# Blah\n\n');
+				expect(makeMarkdown('<h2>Blah</h2>'		)).to.be('## Blah\n\n');
+				expect(makeMarkdown('<h3>Blah</h3>'		)).to.be('### Blah\n\n');
+				expect(makeMarkdown('<h4>Blah</h4>'		)).to.be('#### Blah\n\n');
+				expect(makeMarkdown('<h5>Blah</h5>'		)).to.be('##### Blah\n\n');
+				expect(makeMarkdown('<h6>Blah</h6>'		)).to.be('###### Blah\n\n');
 			});
 			
 		});
